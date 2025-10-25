13c9034432818a1a59a670cb849e36129c79abc2
mariamishinjiashvili <mariamishin03@gmail.com>
2025-10-25 13:05:40 -0400

    Initial commit: add CMPSC472 project

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <string.h>
+#include <semaphore.h>
+#include <time.h>
+int main(){
+    printf("hello project");
+}
